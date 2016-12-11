df686265b1937b85293579c0fdf75c5236639cab
Catherine Claudet <cmclaudet@gmail.com>
2016-12-11 19:47:45 +0100

    Fixed background layer problem
    - Changed z value of background to 10 to ensure sprites are placed above
    background
    - Changed max size of score text to make text adapt to screen size

---
+++ CLOUD10/Assets/Scripts/setBGLayer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setBGLayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        GetComponent<MeshRenderer>().sortingLayerName = "Background";
+	}
+}
